511757edb2dff14ce26df71d20fd54bcf15966cf
WyattFoster4 <123209712+WyattFoster4@users.noreply.github.com>
2023-01-25 15:30:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// MAKE 'GET DISTANCE VALUE' function (will talk to Leo) for trueDistance var
+
+printDistance = 100 - trueDistance // inverse readable as a %
+
+// concatenate with % sign and send to HTML with innerhtml ID
